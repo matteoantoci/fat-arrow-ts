9fe22a08f53e8e7a12a4ae644ef2b2590e628d3c
Matteo Antoci <matteo.antoci@lastminute.com>
2021-01-14 21:45:17 +0100

    Refactor

--- src/lambda.ts
+++ src/lambda.ts
@@ -12,15 +12,11 @@ export const once = <A extends any[], R>(fn: OnceParams<A, R>): OnceParams<A, R>
 	}
 }
 
-export const repeat = <T>(times: number, factory: (index: number) => T) =>
-	Array(times)
-		.fill(null)
-		.map((_, i) => factory(i))
+const createArray = (times: number): undefined[] => Array(times).fill(undefined)
+
+export const repeat = <T>(times: number, factory: (index: number) => T) => createArray(times).map((_, i) => factory(i))
 
 export const rotate = <T>(n: number, arr: T[]): T[] => arr.slice(n, arr.length).concat(arr.slice(0, n))
 
 export const chunk = <T>(size: number, arr: T[]): T[][] =>
-	Array(Math.ceil(arr.length / size))
-		.fill(undefined)
-		.map((_, index) => index * size)
-		.map((begin) => arr.slice(begin, begin + size))
+	repeat(Math.ceil(arr.length / size), (index) => index * size).map((begin) => arr.slice(begin, begin + size))
