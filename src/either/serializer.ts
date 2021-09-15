17efbe993c01541ca446c443f13aaf0b8f4b28b7
Matteo Antoci <matteo.antoci@lastminute.com>
2021-09-15 21:45:56 +0200

    Improved serializer

--- src/either/serializer.ts
+++ src/either/serializer.ts
@@ -3,12 +3,12 @@ import { Serializable, Variants } from '../types'
 
 const isError = (data: any): data is Error => data instanceof Error || Object.getPrototypeOf(data) === Error.prototype
 
-const stringify = (variant: Variants, serialized: string): string => `${variant}(${serialized})`
+const wrap = (variant: Variants, serialized: string): string => `${variant}(${serialized})`
 
-const stringifyError = (data: Error): string => `${data.name}("${data.message}")`
+const stringify = <T>(value: T) => (isError(value) ? `${value.name}("${value.message}")` : JSON.stringify(value))
 
 export const createSerializable = <T>(variant: Variants, value: T): Serializable<T> => ({
-	toString: constant(() => stringify(variant, isError(value) ? stringifyError(value) : JSON.stringify(value))),
+	toString: constant(() => wrap(variant, stringify(value))),
 	toJSON: constant(() => {
 		console.warn(`Either values should not be serialized directly to JSON. Please, consider applying "fold" first.`)
 		return { variant, value }
